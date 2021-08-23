27bbfed0ecac07c1be9dccbcca92f2f29f1e7473
Ksdem <Ksdem>
2021-08-23 13:19:24 +0300

    cycles

--- test.js
+++ test.js
@@ -6,10 +6,30 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a1 = prompt('Один из последних просомтренных фильмв?', ''),
-    a2 = prompt('На сколько оцениваете его?', ''),
-    b1 = prompt('Один из последних просомтренных фильмв?', ''),
-    b2 = prompt('На сколько оцениваете его?', '');
-personalMovieDB.movies[a1] = a2;
-personalMovieDB.movies[b1] = b2;
+for (let i = 0; i < 2; i++) {
+    const a1 = prompt('Один из последних просомтренных фильмв?', ''),
+        a2 = prompt('На сколько оцениваете его?', '');
+
+    if (a1 != null && a2 != null && a1 != '' && a2 != '' && a1.length < 50) {
+        personalMovieDB.movies[a1] = a2;
+    } else {
+
+        i--;
+        console.log('error');
+    }
+
+}
+switch (true) {
+    case personalMovieDB.count < 10:
+        console.log('Просмотрено довольно мало фильмов');
+        break;
+    case personalMovieDB.count > 10 && personalMovieDB.count < 30:
+        console.log('Вы классический зритель');
+        break;
+    case personalMovieDB.count > 30:
+        console.log('Вы киноман');
+        break;
+    default:
+        console.log('Произошла ошибка');
+}
 console.log(personalMovieDB);
